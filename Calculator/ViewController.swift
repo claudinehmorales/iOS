6b1e318fdeb9ad62d955baff43f554e2dd6630fe
Claudine Morales <moralesc@seamoralesc02.webtrends.corp>
2015-07-10 14:14:50 -0700

    calc hw

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Claudine Morales on 7/10/15.
+//  Copyright © 2015 Webtrends Swift School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = 
+    }
+}
+
